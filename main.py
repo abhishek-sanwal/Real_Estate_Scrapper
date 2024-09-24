eb842232f64322925b3ced2142b0c236893b1b32
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-09-24 11:15:00 +0530

    📖Added comments

--- main.py
+++ main.py
@@ -33,68 +33,92 @@ rating_class = ".tupleNew__locRatings span"
 
 def extract_data(url):
 
-    time.sleep(5)
-    
+    default = "Nothing"
+    # Get a chormedriver
     driver = webdriver.Chrome()
     
     driver.get(url)
     
+    # Wait for 10 secs to load
     time.sleep(10)
+    
+    # Move 1000px in y-direction so data gets visible
+    # website is dynamic so it is essential step
     driver.execute_script(
         "window.scrollTo(0, 1000);")
     
+    # Create a dictionary
     mapp = defaultdict(list)
    
+    # keys for our mapp    
     lis = ['society_name', "description", "area", "bhk", "square_ft",
            "price", "possession","rating"]
     
     
     index = 0
+    
+    # Go class by class
     for class_ in [society_class,description_id,area_class, bhk_class, 
                    squft_class, price_class, possession_class, 
                    rating_class]:
         
+        # Fetch each element
         for element in driver.find_elements(By.CSS_SELECTOR,class_):
             
-            
+            # Fetch text and remove whitespaces around it
             text = element.text.strip()
+            # If text exists
             if text:
+                # Append to its key-list
                 mapp[lis[index]].append(text)
                         
 
 
         index += 1
     
+    # Fetch Maximum length of any list
     maxi = max(len(mapp[i]) for i in mapp)
    
+    # Make each list length same    
     for lis in mapp:
         
         if len(mapp[lis]) != maxi:
             
-            mapp[lis] += ['Nothing']*(maxi-len(mapp[lis]))
+            # Add a default word
+            mapp[lis] += [default]*(maxi-len(mapp[lis]))
     
+    # Convert to DF
     df = pd.DataFrame(mapp)
     
+    # Convert to csv and store it
     df.to_csv('99acres.csv')
     
+    # Task done
     print("Task completed")
 
 def extractSocietyData(societyName:str)->None:
-    
     
     driver = webdriver.Chrome()
     
     driver.get("https://www.99acres.com/")
     
+    # Wait for page to get loaded
     time.sleep(5)
     
+    # Extract title of page
     print(driver.title)
     
+    # Scroll page by 1000px in y direction so that search bar will be
+    # visible in viewport
     driver.execute_script("window.scrollBy(0, 1000);") 
     
+    # Now searchbar is visible
     time.sleep(2)
     
+    # Find search bar by its id
     search_bar = driver.find_element(By.ID,"keyword")
+    
+    # Print placeholder text
     print(search_bar.get_attribute('placeholder'))
     
     # Click search bar to enter data
@@ -114,9 +138,14 @@ def extractSocietyData(societyName:str)->None:
     # Press enter search_bar
     search_bar.send_keys(Keys.RETURN)
 
+    # Wait for redirecting to new page.
     time.sleep(10)
+    
     # Error occurs here
     # This page can not be loaded
+    # Server side js has bot-detection scripts
+    # So we need to close this driver
+    # and reload a new one
     url = driver.current_url
     
     driver.close()
@@ -124,6 +153,6 @@ def extractSocietyData(societyName:str)->None:
     extract_data(url)
     
 
-societyName = "Ajnara Genx"
+societyName = input()
 
 print(extractSocietyData(societyName))
