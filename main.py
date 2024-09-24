aa2195a6450b7277a36e10c701cb179a93013919
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-09-24 11:35:43 +0530

    Removed some unused statements

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
-import time,requests
+import time
 import pandas as pd
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
