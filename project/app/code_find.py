6a11159e58ec9407043559f12ec282414466f1c3
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-09-29 17:58:15 +0530

    :zap: Updated names and fixed bugs

--- project/app/code_find.py
+++ project/app/code_find.py
@@ -1,21 +1,21 @@
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from selenium.webdriver.common.by import By
 from collections import defaultdict
 
+import os
 import pandas as pd
 import time
 
+from app.main import get_node,get_driver,LONG_WAIT
+
 # Global Variables
 # UrlS
-city_url :str = "https://99acres.com/search/property/buy?preference=&city="
-locality_url:str = f"{city_url}7&locality="
+CITY_URL :str = "https://99acres.com/search/property/buy?preference=&city="
+LOCALITY_URL:str = f"{CITY_URL}7&locality="
 
 # Class for fetching names
-class_ = ".tags-and-chips__textOnly span:nth-child(2)"
+SEARCH_NAME_class_ = ".tags-and-chips__textOnly span:nth-child(2)"
 
+BASE_DIR = os.path.dirname(__file__)
 
 '''
 A script to find city-code and locality code for all cities ,
@@ -28,33 +28,25 @@ def solve(url,start_count=1,end_count=50,name="city"):
     # Run this loop more times to find more cityCode
     for i in range(start_count,end_count+1):
         
-        try:
-            
-            driver = webdriver.Chrome()
-            time.sleep(5)
-            driver.get(url + str(i))
-            time.sleep(5)
+        driver = get_driver(url)
+        time.sleep(LONG_WAIT)
 
-            element = driver.find_element(By.CSS_SELECTOR, class_)
+        element = get_node(driver, SEARCH_NAME_class_)
 
-            if element and element.text :
-                print(i,element.text) 
-                mapp[name].append(element.text)
-                mapp["index"].append(i)
-                print(mapp)
+        if element and element.text :
+            print(i,element.text) 
+            mapp[name].append(element.text)
+            mapp["index"].append(i)
+            print(mapp)
             
-        except Exception as e:
-            print(e)
-        
         # Server is blocking the 
         driver.close()
-        time.sleep(2)
         
     print(mapp,"this is mapp.")
     df = pd.DataFrame(mapp)
-    
+    relative_path = os.path.join(BASE_DIR,f"../CSV/99acres_{name}_code.csv")
     df.to_csv(
-        f"99acres_{name}_code.csv",mode="a",header=False,index=False
+        relative_path,mode="a",header=False,index=False
     )
 
     return "Done"
@@ -62,7 +54,14 @@ def solve(url,start_count=1,end_count=50,name="city"):
 
 if __name__ =="__main__":
     
-    solve(locality_url,156,205,"locality")
+    url = LOCALITY_URL
+    start_count = 156
+    end_count = 205
+    type_ = "locality"
+    
+    
+    
+    solve(url,start_count,end_count,type_)
     
 
     
