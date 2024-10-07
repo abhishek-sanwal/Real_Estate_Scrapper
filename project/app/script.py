b102f44674f22ceb08692907cf3b0040dcde11a3
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-07 12:12:00 +0530

    :zap: Updated file

--- project/app/script.py
+++ project/app/script.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.by import By
 from collections import defaultdict
 import pandas as pd
 import time
-
-from typing import List,DefaultDict
+from typing import List, DefaultDict
 from exceptions.exception import CityCodeException, LocalityCodeException
 from app.main import process_results
 
@@ -14,67 +13,70 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 
 
+CITY_CODE_CSV = os.path.join(
+    BASE_DIR, "../CSV/city_code/99acres_city_code.csv")
+LOCALITY_CODE_CSV = os.path.join(
+    BASE_DIR, "../CSV/locality_code/99acres_locality_code.csv")
 
-CITY_CODE_CSV = os.path.join(BASE_DIR,"../CSV/city_code/99acres_city_code.csv") 
-LOCALITY_CODE_CSV = os.path.join(BASE_DIR,"../CSV/locality_code/99acres_locality_code.csv")
 
 def solve():
-    
+
     # we can set more filters by adjusting query strings
-    
+
     # Should be in lakhs
-    min_budget = 10 # 10 Lakhs
-    max_budget = 170 # 1 cr 170 lakhs
-    bhk=2 # 2 for 2bhk, 3for 3bhk
-    city = "Noida"
+    min_budget = 10  # 10 Lakhs
+    max_budget = 170  # 1 cr 170 lakhs
+    bhk = 2  # 2 for 2bhk, 3for 3bhk
+    city = "Bareilly"
     locality = "Sector 18 Noida"
     type_ = "Ready_to_move"
-    
+
     # Status of apartment you want
-    available={
-        "New":0,
-        "Under_Construction":1,
-        "Ready_to_move":2
+    available = {
+        "New": 0,
+        "Under_Construction": 1,
+        "Ready_to_move": 2
     }
 
     av = available[type_]
-    
+
     df = pd.read_csv(
-        f"{CITY_CODE_CSV}",index_col=False
+        f"{CITY_CODE_CSV}", index_col=False
     )
 
     loc_df = pd.read_csv(
-        f"{LOCALITY_CODE_CSV}",index_col=False
+        f"{LOCALITY_CODE_CSV}", index_col=False
     )
-    
+
     city_code_df = df[df["city"] == city]
     loc_df = loc_df[loc_df["locality"] == locality]
-    
+
     # If no such city code exists
     if not len(loc_df):
-        
+
         raise LocalityCodeException("Locality not found in df. \
                                     To scrap that locality first add \
                                     that locality code in csv manually or run script")
-    
+
     # if not such locality code exists
-    if not len(city_code_df) :
-        raise CityCodeException("City not found in df. Please choose one of the city from the csv")
-    
+    if not len(city_code_df):
+        raise CityCodeException(
+            "City not found in df. Please choose one of the city from the csv")
+
     # Get value
     city_code = city_code_df["code"].values[0]
-    
+
     # Get value
     locality_code = loc_df["code"].values[0]
-    
+
     '''
     For Scrapping data city wise Use this code
     '''
     # For scrapping results city wise
-    city_url = f"https://99acres.com/search/property/buy?city={city_code}&bedroom_num={bhk}&budget_max={max_budget}&budget_min={min_budget}&availability={av}"
+    city_url = f"https://99acres.com/search/property/buy?city={city_code}&availability={av}"
 
-    process_results(city_url, city,True)
-    
+    process_results(city_url, city, True)
+
     '''
     For Scrapping data city + locality wise use this code
     '''
@@ -84,5 +86,5 @@ def solve():
     # process_results(locality_url, locality,True)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     solve()
